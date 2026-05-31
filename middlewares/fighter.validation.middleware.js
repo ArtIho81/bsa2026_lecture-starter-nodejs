9afba124eb03bd7930b86010f8bf35952ddacdc2
Ihor Artomov <igora2406@gmail.com>
2026-05-31 19:24:28 +0300

    + middlewares

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,12 +1,119 @@
 import { FIGHTER } from "../models/fighter.js";
 
+const valuesToValidate = {
+  name: {
+    value: "name",
+    required: true,
+  },
+  power: {
+    value: "power",
+    range: [1, 100],
+    required: true,
+  },
+  defense: {
+    value: "defense",
+    range: [1, 10],
+    required: true,
+  },
+  health: {
+    value: "health",
+    range: [80, 120],
+    required: false,
+  },
+};
+
+const checkUniqueValue = (field, value, id) => {
+  const isValueExist = fighterService.search({ [field]: value.toLowerCase() });
+  const unique = !isValueExist || isValueExist.id === id;
+  return unique;
+};
+
+const validateProperty = (value = 85, field) => {
+  const [min, max] = valuesToValidate[field].range;
+  return value >= min && value <= max;
+};
+
+const isEmptyValue = (data) =>
+  !(typeof data === "number" ? true : data?.length);
+
 const createFighterValid = (req, res, next) => {
-  // TODO: Implement validatior for FIGHTER entity during creation
+  const candidate = req.body;
+  const errors = [];
+
+  Object.keys(FIGHTER).forEach((field) => {
+    if (
+      field !== "id" &&
+      !valuesToValidate[field].required &&
+      isEmptyValue(candidate[field])
+    ) {
+      errors.push(`${field} is required`);
+    } else if (field === valuesToValidate.name.value) {
+      const unique = checkUniqueValue(field, candidate[field]);
+      if (!unique) {
+        errors.push(`${field} already exist`);
+      }
+    } else if (
+      (field === valuesToValidate.power.value ||
+        field === valuesToValidate.defense.value ||
+        field === valuesToValidate.health.value) &&
+      !validateProperty(candidate[field], field)
+    ) {
+      errors.push(`${field} isn't valid`);
+    }
+  });
+
+  if ("id" in candidate) {
+    errors.push("Id in the request body is present");
+  }
+
+  if (errors.length > 0) {
+    res.err = errors;
+    return responseMiddleware(req, res, next);
+  }
+
   next();
 };
 
 const updateFighterValid = (req, res, next) => {
-  // TODO: Implement validatior for FIGHTER entity during update
+  const updatedData = req.body;
+  const { id } = req.params;
+  const fighter = fighterService.search({ id });
+  if (!fighter) {
+    res.err = new Error(`Fighter wasn't found`);
+    return responseMiddleware(req, res, next);
+  }
+
+  const errors = [];
+  const updatedFieldsNumber = Object.keys(FIGHTER).filter(
+    (field) => field !== "id" && !isEmptyValue(updatedData[field]),
+  );
+  if (updatedFieldsNumber.length === 0) {
+    errors.push("No data to update");
+  } else {
+    updatedFieldsNumber.forEach((update) => {
+      if (
+        (field === valuesToValidate.power.value ||
+          field === valuesToValidate.defense.value ||
+          field === valuesToValidate.health.value) &&
+        !validateProperty(updatedData[field], field)
+      ) {
+        errors.push(`${update} isn't valid`);
+      } else if (update === valuesToValidate.name.value) {
+        const unique = checkUniqueValue(update, updatedData[update], id);
+        if (!unique) {
+          errors.push(`${update} already exist`);
+        }
+      }
+    });
+  }
+  if ("id" in updatedData) {
+    errors.push("Id in the request body is present");
+  }
+  if (errors.length > 0) {
+    res.err = errors;
+    return responseMiddleware(req, res, next);
+  }
+
   next();
 };
 
--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,12 +1,117 @@
 import { USER } from "../models/user.js";
+import { userService } from "../services/userService.js";
+import { responseMiddleware } from "./response.middleware.js";
+
+const valuesToValidate = {
+  email: "email",
+  password: "password",
+  phone: "phone",
+};
+
+const validators = {
+  email: (email) => /^[a-zA-Z0-9._%+-]+@gmail\.com$/.test(email.trim()),
+  phone: (phone) => /^\+380[\d]{9}$/.test(phone.trim()),
+  password: (password) => password.trim().length >= 3,
+};
+
+const checkUniqueValue = (field, value, id) => {
+  const isValueExist = userService.search({ [field]: value.toLowerCase() });
+  const unique = !isValueExist || isValueExist.id === id;
+  return unique;
+};
+
+const validateUniqueValue = (field, value, id) => {
+  let valid;
+  if (field === valuesToValidate.email) {
+    valid = validators.email(value);
+  } else if (field === valuesToValidate.phone) {
+    valid = validators.phone(value);
+  }
+  if (!valid) {
+    return `${field} isn't valid`;
+  }
+  const unique = checkUniqueValue(field, value, id);
+  if (!unique) {
+    return `${field} already exist`;
+  }
+  return null;
+};
 
 const createUserValid = (req, res, next) => {
-  // TODO: Implement validatior for USER entity during creation
+  const candidate = req.body;
+  const errors = [];
+
+  Object.keys(USER).forEach((field) => {
+    if (field !== "id" && !candidate[field]?.trim()) {
+      errors.push(`${field} is required`);
+    } else if (
+      field === valuesToValidate.password &&
+      !validators.password(candidate[field])
+    ) {
+      errors.push(`${field} isn't valid`);
+    } else if (
+      field === valuesToValidate.email ||
+      field === valuesToValidate.phone
+    ) {
+      const error = validateUniqueValue(field, candidate[field]);
+      if (error) {
+        errors.push(error);
+      }
+    }
+  });
+
+  if ("id" in candidate) {
+    errors.push("Id is present in the request body");
+  }
+
+  if (errors.length > 0) {
+    res.err = errors;
+    return responseMiddleware(req, res, next);
+  }
   next();
 };
 
 const updateUserValid = (req, res, next) => {
-  // TODO: Implement validatior for user entity during update
+  const updatedData = req.body;
+  const { id } = req.params;
+  const user = userService.search({ id });
+  if (!user) {
+    res.err = new Error(`User wasn't found`);
+    return responseMiddleware(req, res, next);
+  }
+
+  const errors = [];
+  const updatedFields = Object.keys(USER).filter(
+    (field) => field !== "id" && updatedData[field] !== 0,
+  );
+  if (updatedFields.length === 0) {
+    errors.push("No data to update");
+  } else {
+    updatedFields.forEach((update) => {
+      if (
+        update === valuesToValidate.password &&
+        !validators.password(updatedData[update])
+      ) {
+        errors.push(`${field} isn't valid`);
+      } else if (
+        update === valuesToValidate.email ||
+        update === valuesToValidate.phone
+      ) {
+        const error = validateUniqueValue(update, updatedData[update], id);
+        if (error) {
+          errors.push(error);
+        }
+      }
+    });
+  }
+  if ("id" in updatedData) {
+    errors.push("Id is present in the request body");
+  }
+
+  if (errors.length > 0) {
+    res.err = errors;
+    return responseMiddleware(req, res, next);
+  }
   next();
 };
 
